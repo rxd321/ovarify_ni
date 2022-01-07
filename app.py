888100ac708a3362ef6eb2a7959e8db984c6896b
rxd321 <68972156+rxd321@users.noreply.github.com>
2022-01-06 20:10:52 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Use pickle to load in the pre-trained model
-filename = "model_77.pkl"
+filename = "model_7_rf.pkl"
 model = pickle.load(open(filename, "rb"))
 
 # Set up the main route
