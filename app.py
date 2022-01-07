6b46afd8557d2940366e22297ba859bbf13f9b2b
rxd321 <68972156+rxd321@users.noreply.github.com>
2022-01-06 21:35:00 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import pandas as pd
 # Initialise the Flask app
 app = Flask(__name__)
 
-# Use pickle to load in the pre-trained model
-filename = "model_7_xgb.pkl"
-model = pickle.load(open(filename, "rb"))
-
 # Set up the main route
 @app.route('/', methods=["GET", "POST"])
 def main():
@@ -23,18 +19,34 @@ def main():
         meno = request.form.get("meno")
         ca125 = request.form.get("ca125")
         alb = request.form.get("alb")
+        he4 = request.form.get("he4")
+        glo = request.form.get("glo")
+        lym = request.form.get("lym")
         
-        # Create DataFrame based on input
-        input_variables = pd.DataFrame([[age, cea, ibil,neu,meno,ca125,alb]],
-                                       columns=['Age','CEA','IBIL','NEU','Menopause','CA125','ALB'],
+        if he4 == None:
+            # Use pickle to load in the pre-trained 9 features model
+            filename = "model_9_xgb.pkl"
+            model = pickle.load(open(filename, "rb"))
+            input_variables = pd.DataFrame([[age, cea, ibil,neu,meno,ca125,alb,glo,lym]],
+                                       columns=['Age','CEA','IBIL','NEU','Menopause','CA125','ALB','GLO','LYM%'],
                                        dtype=float,
                                        index=['input'])
+        else
+            # Use pickle to load in the pre-trained 10 features model
+            filename = "model.pkl"
+            model = pickle.load(open(filename, "rb"))
+            # Create DataFrame based on input
+            input_variables = pd.DataFrame([[age, cea, ibil,neu,meno,ca125,alb,he4,glo,lym]],
+                                       columns=['Age','CEA','IBIL','NEU','Menopause','CA125','ALB','HE4','GLO','LYM%'],
+                                       dtype=float,
+                                       index=['input'])
+
         
         # Get the model's prediction
         # Given that the prediction is stored in an array we simply extract by indexing
         prediction = model.predict(input_variables)
     
         # We now pass on the input from the from and the prediction to the index page
-        return render_template("index.html", original_input={'Age':age,'CEA':cea,'IBIL':ibil,'NEU':neu,'Menopause':meno,'CA125':ca125,'ALB':alb}, result=True, prediction=prediction)
+        return render_template("index.html", original_input={'Age':age,'CEA':cea,'IBIL':ibil,'NEU':neu,'Menopause':meno,'CA125':ca125,'ALB':alb,'HE4':he4,'GLO':glo,'LYM%':lym}, result=True, prediction=prediction)
     # If the request method is GET
     return render_template("index.html", result=False)
